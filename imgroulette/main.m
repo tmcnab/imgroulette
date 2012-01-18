2a5ffa586d489d121222e258570e9fd1c2937bfe
Solidus <tristan@seditious-tech.com>
2012-01-17 20:11:48 -0500

    Initial Commit

---
+++ imgroulette/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  imgroulette
+//
+//  Created by Tristan McNab on 17/01/12.
+//  Copyright Seditious Technologies 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
